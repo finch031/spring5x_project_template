2a7dda777ce7c6ff1b65a8114b9ede2ea9ed8f01
余胜 <yusheng@intest.cn>
2020-10-03 21:26:04 +0800

    update

--- src/main/java/com/yee/spring5x/Spring5xApplication.java
+++ src/main/java/com/yee/spring5x/Spring5xApplication.java
@@ -2,6 +2,7 @@ package com.yee.spring5x;
 
 import com.yee.spring5x.entity.Axe;
 import com.yee.spring5x.entity.Person;
+import com.yee.spring5x.utils.NanoTimer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -14,17 +15,25 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Spring5xApplication {
 
     public static void main(String[] args){
+        NanoTimer nanoTimer = new NanoTimer();
+
         ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+        System.out.println("duration-1: " + nanoTimer.reset());
 
         System.out.println(ctx.getId());
         System.out.println(ctx.getStartupDate());
         System.out.println(ctx.getDisplayName());
         System.out.println(ctx.getApplicationName());
 
+        System.out.println("duration-2: " + nanoTimer.reset());
+
         Axe axe = ctx.getBean(Axe.class);
         System.out.println(axe.chop());
 
         Person person = (Person) ctx.getBean("person");
         person.useAxe();
+
+
+
     }
 }
